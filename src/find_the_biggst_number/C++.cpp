dd165a4046624c7b4a848e90d5d66a54d88b3fe6
DevolperPHP <devolperphp@gmail.com>
2020-05-02 02:43:22 +0300

    Update C++.cpp

--- src/find_the_biggst_number/C++.cpp
+++ src/find_the_biggst_number/C++.cpp
@@ -10,6 +10,6 @@ int main() {
   } else if(num2 >= num1 && num2 >= num3){
     cout<<num2<<" is the biggest number";
   } else if(num3 >= num1 && num3 >= num2){
-    cout<<num3<<" is the biggest number";
+    cout<<num3<<" is the Minimum number";
   }
 }
