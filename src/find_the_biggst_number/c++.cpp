b361746b500ca23cb05f7b9ea1ab75d20cf8a863
DevolperPHP <devolperphp@gmail.com>
2020-05-02 02:12:19 +0300

    Create c++.cpp

---
+++ src/find_the_biggst_number/c++.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int num1 = 23;
+  int num2 = 235;
+  int num3 = 2677;
+
+  if(num1 >= num2 && num1 >= num3){
+    cout<<num1<<" is the biggest number";
+  } else if(num2 >= num1 && num2 >= num3){
+    cout<<num2<<" is the biggest number";
+  } else if(num3 >= num1 && num3 >= num2){
+    cout<<num3<<" is the biggest number";
+  }
+}
