d2edd4c4c62e6b4e8f76a84bc5dd5c35558e99d5
DevolperPHP <devolperphp@gmail.com>
2020-05-02 03:39:37 +0300

    Add files via upload

---
+++ src/find_the_minimum_number/Node.js
@@ -0,0 +1,15 @@
+var num1 = 123;
+var num2 = 4432;
+var num3 = 234;
+
+if(num1 <= num2 && num1 <= num3){
+  console.log(num1)
+}
+
+if(num2 <= num1 && num2 <= num3){
+  console.log(num2)
+}
+
+if(num3 <= num1 && num3 <= num2){
+  console.log(num2)
+}
