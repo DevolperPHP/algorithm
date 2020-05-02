fc5493eaf849c6aa98bc09e67796d5316c1c7f74
DevolperPHP <devolperphp@gmail.com>
2020-05-02 03:38:57 +0300

    Add files via upload

---
+++ src/find_the_minimum_number/JavaScript.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<p>Click the button to return the lowest number of 5 and 10.</p>
+
+<button onclick="myFunction()">Try it</button>
+
+<p id="demo"></p>
+
+<script>
+function myFunction() {
+  document.getElementById("demo").innerHTML = Math.min(335, 20);
+}
+</script>
+
+</body>
+</html>
