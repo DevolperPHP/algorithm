28ddb65d1dbed3ef3caef62111cdc290b185832a
DevolperPHP <devolperphp@gmail.com>
2020-05-02 23:03:45 +0300

    Create PHP.php

---
+++ src/Palindrome/PHP.php
@@ -0,0 +1,39 @@
+//using strrev function
+function palindrome($str){
+  if(strtolower($str) === strtolower(strrev($str))){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+if(palindrome("Malayalam")){
+  echo "its Plaindrome\n";
+}else {
+  echo "its not Plaindrome\n";
+}
+
+
+
+
+
+//using loop
+function palindrome2($str){
+  $strrev = "";
+  
+  for($i = 1; $i <= strlen($str); $i++){
+    $strrev .= substr($str, strlen($str) - $i, 1);
+  }
+  
+  if(strtolower($str) === strtolower($strrev)){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+if(palindrome("huh")){
+  echo "its Plaindrome\n";
+}else {
+  echo "its not Plaindrome\n";
+}
