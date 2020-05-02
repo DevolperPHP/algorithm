f0afc474319decaf5a1dfa375b4b69311af82ac6
DevolperPHP <devolperphp@gmail.com>
2020-05-02 23:58:26 +0300

    Update Python.py

--- src/Palindrome/Python.py
+++ src/Palindrome/Python.py
@@ -1,16 +1,16 @@
-def isPalindrome(s): 
+def isPalindrome(s):    نسوي الفنكشن نعرف بيه المطلوب
 
 	rev = ''.join(reversed(s)) 
   
-	if (s == rev): 
+	if (s == rev): نعطي الشرط اذا تطابقة القيمة مع معاكسها
 		return True
 	return False
 
 # main function 
-s = "malayalam"
-ans = isPalindrome(s) 
+s = "malayalam" هذي هي القيمة
+ans = isPalindrome(s)  نقله نجرب القيمة اذا توافقت او لا
 
 if (ans): 
-	print("Yes") 
+	print("Yes") اذا توافقت نقله نعم
 else: 
-	print("No") 
+	print("No") واذا ما توافقت نحط لا
